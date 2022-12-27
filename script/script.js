a4bbaa171452f06a0b8ce55fc67f8cc6b22a7a10
Adept-Ozzy <99928949+Adept-Ozzy@users.noreply.github.com>
2022-12-27 08:25:23 -0500

    parameters for the player's choice are set

--- script/script.js
+++ script/script.js
@@ -4,7 +4,34 @@ let playerScore = 0;
 let computerScore = 0;
 let roundwinner = '';
 
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        if (playerSelection =="rock"){
+            playerChoice = 0;
+        }
+        if (playerSelection =="paper"){
+            playerChoice = 1;
+        }
+        if (playerSelection =="scissors"){
+            playerChoice = 2;
+        }
+
+        getComputerChoice();
+        playGame();
+    })
+})
+
 function getComputerChoice(){
+    randomChoice = Math.floor(Math.random() * 3);
+        if(randomValue == "0") {
+            return "rock";
+        }
+        if(randomValue == "1") {
+            return "paper";
+        }
+        if(randomValue == "2") {
+            return "scissors";
+        }
 
 }
 function playRound(playerSelection, computerSelection) {
@@ -13,24 +40,24 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-    (playerSelection === 'SCISSORS' && computerSelection ==='PAPER') ||
-    (playerSelection === 'PAPER' && computerSelection === 'ROCK')){
-        playerScore++;
-        roundwinner = 'player';
+        (playerSelection === 'SCISSORS' && computerSelection ==='PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')){
+            playerScore++;
+            roundwinner = 'player';
     }
 
     if((playerSelection === 'SCISSORS' && computerSelection === 'ROCK') ||
-    (playerSelection === 'PAPER' && computerSelection ==='SCISSORS') ||
-    (playerSelection === 'ROCK' && computerSelection === 'PAPER')){
-        computerScore++;
-        roundwinner = 'computer';
+        (playerSelection === 'PAPER' && computerSelection ==='SCISSORS') ||
+        (playerSelection === 'ROCK' && computerSelection === 'PAPER')){
+            computerScore++;
+            roundwinner = 'computer';
     }
 
 
 
 }
 
-const playerSelection = "rock";
+const playerSelection = button.id;
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
