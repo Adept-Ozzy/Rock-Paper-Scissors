3b7961ca00515d76e0f11de2c8d09947400c0815
Adept-Ozzy <99928949+Adept-Ozzy@users.noreply.github.com>
2022-12-27 07:50:38 -0500

    win conditions for computer and player set

--- script/script.js
+++ script/script.js
@@ -1,10 +1,41 @@
+// 5 round game of Rock-Paper-Scissors
+
+let playerScore = 0;
+let computerScore = 0;
+let roundwinner = '';
+
 function getComputerChoice(){
 
 }
 function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        roundwinner = 'tie';
+    }
+
+    if((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection ==='PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')){
+        playerScore++;
+        roundwinner = 'player';
+    }
+
+    if((playerSelection === 'SCISSORS' && computerSelection === 'ROCK') ||
+    (playerSelection === 'PAPER' && computerSelection ==='SCISSORS') ||
+    (playerSelection === 'ROCK' && computerSelection === 'PAPER')){
+        computerScore++;
+        roundwinner = 'computer';
+    }
+
+
 
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+function game(playRound){
+    for (let i=0; i<=5; i++){
+
+    }
+}
